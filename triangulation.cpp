84dba85e06241ff0daa1350c1d4298f82cc2f1a2
Allen Xiao <xiao.allen@berkeley.edu>
2012-08-31 21:28:08 -0700

    link status (should change to NULL/GHOST)

--- triangulation.cpp
+++ triangulation.cpp
@@ -1,6 +1,8 @@
 
 
 #include "triangulation.h"
+#include "geometric.h"
+#include <math.h>
 #include <iostream>
 #include <vector>
 
@@ -8,12 +10,12 @@ using namespace std;
 
 template <typename T> 
 Triangulation<T>::Triangulation() {
-    this->linkrings = new map<T, Linkring*>();
+    this->linkrings = new map<T, Linkring<T>*>();
 }
 
 template <typename T> 
 Triangulation<T>::~Triangulation() {
-    typename map<T, Linkring*>::iterator start, end, it; 
+    typename map<T, Linkring<T>*>::iterator start, end, it; 
     start = this->linkrings->begin();
     end = this->linkrings->end();
     for (it = start; it != end; ++it) {
@@ -28,8 +30,9 @@ void Triangulation<T>::init(Iter start, Iter end, Comp comp) {
     // comp is a spatial comparison function, eg atan2
     cout << "hi" << endl; 
     this->comparator = comp;
+    pair<T, Linkring<T>*> lrpair;
     for (Iter it = start; it != end; ++it) {
-        pair<T, Linkring*> lrpair = pair<T, Linkring*>(*it, new Linkring()); 
+        lrpair = pair<T, Linkring<T>*>(*it, new Linkring<T>()); 
         this->linkrings->insert(lrpair);
     }
 }
@@ -47,42 +50,56 @@ void Triangulation<T>::add_triangle(const T& u, const T& v, const T& w) {
 
 template <typename T>
 void Triangulation<T>::rm_triangle(const T& u, const T& v, const T& w) {
-    // TODO
+    // update u's linkring
+    rm_from_star(u, v, w);
+    // update v's linkring
+    rm_from_star(v, w, u);
+    // update w's linkring
+    rm_from_star(w, u, v);
 }
 
 
 // private methods
 
-// helper function to add new vertices to a link ring
 template <typename T> 
-void Triangulation<T>::add_to_star(const T& sv, const T& v1, const T& v2) {
-    vector<T>* star;
-    typename vector<T>::iterator it, pos1, pos2;
+void Triangulation<T>::add_to_star(const T& sv, const T& v1, const T& v2) { 
+    // not implemented
+}
+template <typename T> 
+void Triangulation<T>::rm_from_star(const T& sv, const T& v1, const T& v2) {
+    // not implemented
+}
+/*
+// helper function to add new vertices to a link ring
+template<>
+void Triangulation<Point2>::add_to_star(const Point2& sv, const Point2& v1, const Point2& v2) {
+    // TODO: reject if triangle already exists on any of the edges
+    vector<Point2>* star;
+    //map<Point2, Linkring<Point2>*> lr;
+    map<Point2, Linkring<Point2>*>* lr;
+    typename vector<Point2>::iterator it, pos1, pos2;
     bool contain1, contain2;
-    star = this->linkrings[sv]->star;
-    pos1 = find(star.begin(), star.end(), v1);
-    pos2 = find(star.begin(), star.end(), v2);
-    contain1 = pos1 != star.end();
-    contain2 = pos2 != star.end();
+    lr = this->linkrings;
+    star = new vector<Point2>();
+    //star = this->linkrings[sv]->star;
+    pos1 = find(star->begin(), star->end(), v1);
+    pos2 = find(star->begin(), star->end(), v2);
+    contain1 = pos1 != star->end();
+    contain2 = pos2 != star->end();
     if (!(contain1 && contain2)) {
-        // neither of the other vertices are  connected! 
-        // we'll attach both and use the 
-        // spatial sorting function that was provided in init().
-        star->push_back(v1); 
-        star->push_back(v2); 
-        sort(star.begin(), star.end(), this->comparator);
+        
     } else if (contain1) {
-        star->insert(pos1+1, v2);
+
     } else if (contain2) {
-        star->insert(pos2, v1);
+
     }
 }
 
-template <typename T> 
-void Triangulation<T>::rm_from_star(const T& sv, const T& v1, const T& v2) {
-    // TODO
+template<>
+void Triangulation<Point2>::rm_from_star(const Point2& sv, const Point2& v1, const Point2& v2) {
+    // TODO: reject if triangle doesn't exist
 }
-
+*/
 // testing only
 #include "geometric.h"
 bool comp(const int& x, const int& y) { return true; }
@@ -92,6 +109,6 @@ int main() {
     vector<int> v (a, a+5);
     t->init(v.begin(), v.end(), comp);
     delete t;
-    
+
     return 1;
 }
--- triangulation.h
+++ triangulation.h
@@ -10,6 +10,157 @@
 #include <vector>
 #include <map>
 
+
+template <typename T>
+class Link // an individual link in the cyclic data structure
+{
+    public:
+        Link() {
+            val = 0;
+            linkprev = false; 
+            linknext = false; 
+            prev = 0;
+            next = 0;
+        }
+        Link(const T& value) {
+            val = value;
+            linkprev = false; 
+            linknext = false; 
+            prev = 0;
+            next = 0;
+        }
+        ~Link() { }
+        void link_prev() {
+            if (prev != 0) {
+                linkprev = true;
+            }
+        }
+        void link_next() {
+            if (next != 0) {
+                linknext = true;
+            }
+        }
+        void unlink_prev() {
+            linkprev = false;
+        }
+        void unlink_next() {
+            linknext = false;
+        }
+    //private:
+        T val;
+        bool linkprev; 
+        bool linknext;
+        Link<T>* prev;
+        Link<T>* next;
+};
+
+// TODO REDO with Links to include NULL for holes, GHOST for hull
+// class for link rings (see Blelloch et al)
+template <typename T>
+class Linkring
+{
+    public:
+        Linkring() {
+            empty = true;
+            size = 0;
+        }
+        ~Linkring() {
+            if (!empty) {
+                Link<T>* nxt;
+                Link<T>* it = star;
+                do {
+                    nxt = it->next;
+                    delete it;
+                    it = nxt;
+                } while (it != star);
+            }
+        }
+        int locate(const T& v) {
+            // returns the index of v, or -1 if it can't find it
+            if (empty) {
+                return -1;
+            }
+            Link<T>* target = star;
+            int ret = 0;
+            do {
+                if (target->val == v) {
+                    return ret;
+                }
+                target = target->next; 
+                ++ret;
+            } while (target != star);
+            return -1;
+        }
+        void add_link(const T& v, int index) {
+            // insert a Link for v before index
+            if (empty) {
+                star = new Link<T>(v);
+                empty = false;
+            } else {
+                Link<T>* target = star;
+                for (int i = 0; i < index; ++i) {
+                    target = target->next;
+                } 
+                // target and target->prev should not be linked
+                target->prev->next = new Link<T>(v);
+                target->prev = target->prev->next;
+            }
+            ++size;
+        }
+        void del_link(int index) {
+            // delete the link at index
+            // for most purposes, only del_link() unlinked nodes
+            Link<T>* target = star;
+            for (int i = 0; i < index; ++i) {
+                target = target->next;
+            }
+            if ((target->next != 0) && (target->prev != 0)) {
+                target->next->unlink_prev(); 
+                target->next->prev = target->prev;
+                target->prev->unlink_next(); 
+                target->prev->next = target->next;
+            }
+            delete target;
+            size--;
+            if (size == 0) {
+                empty = true;
+            }
+        }
+        void connect_next(int index) {
+            // links the Link at index to its next
+            Link<T>* target = star;
+            for (int i = 0; i < index; ++i) {
+                target = target->next;
+            }
+            target->link_next();
+            target->next->link_prev();
+        }
+        bool linked(const T& v1, const T& v2) {
+            // returns true if there is a positive triangle this, v1, v2
+            // ie the Links for v1 and v2 are linked
+            Link<T>* target = star;
+            do {
+                if (target->val == v1) {
+                    if (target->next->val == v2) {
+                        if (target->linknext && target->next->linkprev) {
+                            return true;
+                        }
+                    } 
+                    return false;
+                } else {
+                    target = target->next;
+                }
+            } while (target != star);
+            return false;
+        }
+        bool empty;
+        int size;
+        Link<T>* star;
+};
+
+#endif
+
+
 // main triangulation class
 template <typename T>
 class Triangulation
@@ -28,46 +179,12 @@ class Triangulation
         T adjacent(const T&, const T&);
         // omitted: adjacent_one()
     private:
-        // class for link rings (see Blelloch et al)
-        class Linkring
-        {
-            public:
-                Linkring() {
-                    this->empty = true;
-                    this->star = new typename std::vector<T>();
-                }
-                ~Linkring() {
-                    delete this->star;
-                }
-                T& operator[](const int idx) {
-                    if (!this->empty) {
-                        return (*star)[idx % star.size()];
-                    }
-                }
-                // inserts an element before index idx
-                void insert(const int idx, const T& x) {
-                    star->insert(idx % star->size(), x);
-                    this->empty = false;
-                }
-                // removes the element at index idx
-                void remove(const int idx) {
-                    if (!this->empty) {
-                        star->erase(idx % star->size());
-                        if (star->empty()) {
-                            this->empty = true;
-                        }
-                    }
-                }
-            //private:
-                bool empty;
-                typename std::vector<T>* star;
-        };
         // link rings around each vertex completely define all edges
         // and positional relationships in the triangulation
         void add_to_star(const T&, const T&, const T&);
         void rm_from_star(const T&, const T&, const T&);
-        typename std::map<T, Linkring*>* linkrings;
+        typename std::map<T, Linkring<T>*>* linkrings;
         bool (*comparator)(const T&, const T&);
 };
 
-#endif
+
